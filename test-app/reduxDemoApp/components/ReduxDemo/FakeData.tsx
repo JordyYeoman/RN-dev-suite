6f70bc829c3f8a099d3921147ad2d97d1d8bc45c
Jordy Yeoman <jordyyeoman@gmail.com>
2022-07-25 18:51:04 +0800

    Extract inline styles

--- test-app/reduxDemoApp/components/ReduxDemo/FakeData.tsx
+++ test-app/reduxDemoApp/components/ReduxDemo/FakeData.tsx
@@ -1,18 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 function FakeData() {
-  const [data, setData] = useState<any>([
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-    getRandomNumberFromRange(10, 1000),
-  ]);
+  const [data, setData] = useState<any>([getRandomNumberFromRange(10, 1000)]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -26,16 +16,9 @@ function FakeData() {
   return (
     <View>
       <Text>FakeData</Text>
-      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+      <View style={styles.bubblesContainer}>
         {data.map((d: number, index: number) => (
-          <Text
-            style={{
-              backgroundColor: 'white',
-              padding: 4,
-              margin: 10,
-              borderRadius: 25,
-            }}
-            key={index}>
+          <Text style={styles.textBubble} key={index}>
             {d}
           </Text>
         ))}
@@ -49,3 +32,13 @@ export default FakeData;
 const getRandomNumberFromRange = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+const styles = StyleSheet.create({
+  bubbleContainer: {flexDirection: 'row', flexWrap: 'wrap'},
+  textBubble: {
+    backgroundColor: 'white',
+    padding: 4,
+    margin: 10,
+    borderRadius: 25,
+  },
+});
